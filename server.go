19c6f9973db8a34a566ccfce8c607c0801789bd7
geneses.lopes <geneses.lopes.terceirizado@mj.gov.br>
2021-03-20 17:25:15 -0300

    feat: :wrench: Configmap family

--- server.go
+++ server.go
@@ -3,9 +3,12 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
+	http.HandleFunc("/configmap",ConfigMap)
 	http.HandleFunc("/",Hello)
 	http.ListenAndServe(":8000",nil)
 }
@@ -15,4 +18,14 @@ func Hello(w http.ResponseWriter, r *http.Request){
 	age := os.Getenv("AGE")
 
 	fmt.Fprintf(w,"Hello I'm %s. I'm %s.",name,age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request){
+	
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+
+	if err != nil {
+		log.Fatalf("Error reading file: ",err)
+	}
+	fmt.Fprintf(w,"My family: %s.",string(data))
 }
