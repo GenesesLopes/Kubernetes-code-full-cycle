28fd436f54e9df3c50cf4c2abd9897b826844738
geneses.lopes <geneses.lopes.terceirizado@mj.gov.br>
2021-03-20 17:41:14 -0300

    feat: :lock: Secrets para variaveis de ambiente

--- server.go
+++ server.go
@@ -9,8 +9,16 @@ import (
 
 func main() {
 	http.HandleFunc("/configmap",ConfigMap)
+	http.HandleFunc("/secret",Secret)
 	http.HandleFunc("/",Hello)
 	http.ListenAndServe(":8000",nil)
+}
+
+func Secret(w http.ResponseWriter, r *http.Request){
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w,"User: %s. Password: %s",user,password)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request){
